cdf0caf8884cabd09e26d2320608a12335eb0ddc
Sergey <malyshevs_v@mail.ru>
2021-03-24 00:32:49 +0300

    timer two

--- src/main/java/ru/smal/MainConsole.java
+++ src/main/java/ru/smal/MainConsole.java
@@ -1,21 +1,24 @@
 package ru.smal;
 
+import javafx.animation.Timeline;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainConsole {
     public static void main(String[] args) {
 
+
         Timer timer = new Timer("Timer");
 
         TimerTask timerTask = new TimerTask() {
-            int count = 1;
+            int count = 15;
             @Override
             public void run() {
 
-                System.out.printf("running %d\n", count++);
-                if(count == 15){
-                    System.out.println("it's work");
+                System.out.printf("running %d\n", count--);
+                if(count == 0){
+                    System.out.println("stop");
                     timer.cancel();
                 }
             }
@@ -23,6 +26,8 @@ public class MainConsole {
 
 //        timer.schedule(timerTask, 0, 1000);
 
-        timer.scheduleAtFixedRate(timerTask, 0, 1000);
+        timer.schedule(timerTask, 0, 1000);
+
+
     }
 }
